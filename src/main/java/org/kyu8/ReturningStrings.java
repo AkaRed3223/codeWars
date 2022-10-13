ed18455cb5bc22ecc7b200b60b6e54462c4d3b56
AkaRed3223 <cesarsales22@gmail.com>
2022-10-13 02:02:34 -0300

    Returning Strings

---
+++ src/main/java/org/kyu8/ReturningStrings.java
@@ -0,0 +1,24 @@
+package org.kyu8;
+
+/*
+ * 8 kyu - Returning Strings
+ *
+ * https://www.codewars.com/kata/55a70521798b14d4750000a4
+ *
+ * Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+ *
+ * [Make sure you type the exact thing I wrote or the program may not execute properly]
+ *
+ */
+
+public class ReturningStrings {
+
+    public static String greet(String name) {
+        return String.format("Hello, %s how are you doing today?", name);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet("Cesar"));
+    }
+}
+
