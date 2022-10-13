18f590825c85bd6b46d8fdb0569bb5f8c2757a13
AkaRed3223 <cesarsales22@gmail.com>
2022-10-13 03:01:54 -0300

    Even or Odd

---
+++ src/main/java/org/kyu8/EverOrOdd.java
@@ -0,0 +1,22 @@
+package org.kyu8;
+
+/*
+ * 8 kyu - Even or Odd
+ *
+ * https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+ *
+ * Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ *
+ */
+
+public class EverOrOdd {
+
+    public static String even_or_odd(int number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(even_or_odd(1236));
+    }
+}
+
