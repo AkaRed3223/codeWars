c01cb70f9f25907a809698963d53197badce4a24
AkaRed3223 <cesarsales22@gmail.com>
2022-10-13 02:47:40 -0300

    Calculate Average

---
+++ src/main/java/org/kyu8/CalculateAverage.java
@@ -0,0 +1,28 @@
+package org.kyu8;
+
+/*
+ * 8 kyu - Calculate average
+ *
+ * https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/
+ *
+ * Write a function which calculates the average of the numbers in a given list
+ *
+ * Note: Empty arrays should return 0
+ *
+ */
+
+import java.util.Arrays;
+
+public class CalculateAverage {
+
+    public static double find_average(int[] array) {
+        if (array.length == 0) return 0;
+        return Arrays.stream(array).sum() / (double) array.length;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        System.out.println(find_average(array));
+    }
+}
+
