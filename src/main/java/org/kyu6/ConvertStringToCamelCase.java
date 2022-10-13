d504a1f800277621f271f84f74f86e960f218d9b
AkaRed3223 <cesarsales22@gmail.com>
2022-10-13 01:50:47 -0300

    clear <p>

--- src/main/java/org/kyu6/ConvertStringToCamelCase.java
+++ src/main/java/org/kyu6/ConvertStringToCamelCase.java
@@ -2,11 +2,11 @@ package org.kyu6;
 
 /**
  * 6 kyu - Convert string to camel case
- * <p></p>
+ *
  * <a href="https://www.codewars.com/kata/517abf86da9663f1d2000003">...</a>
- * <p>
+ *
  * Details:
- * <p>
+ *
  * Complete the method/function so that it converts dash/underscore delimited words into camel
  * casing. The first word within the output should be capitalized only if the original word was
  * capitalized (known as Upper Camel Case, also often referred to as Pascal case).
--- src/main/java/org/kyu6/CreatePhoneNumber.java
+++ src/main/java/org/kyu6/CreatePhoneNumber.java
@@ -2,14 +2,14 @@ package org.kyu6;
 
 /**
  * 6 kyu - Convert String to Camel Case
- * <p></p>
+ *
  * <a href="https://www.codewars.com/kata/517abf86da9663f1d2000003">...</a>
- * <p></p>
+ *
  * Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
  * Example
  * Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
  * The returned format must be correct in order to complete this challenge.
- * <p></p>
+ *
  * Don't forget the space after the closing parentheses!
  */
 
