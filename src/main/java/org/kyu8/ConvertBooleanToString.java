a9ae76399da31ddd1fee2fe11fa56a2997ec8ff4
AkaRed3223 <cesarsales22@gmail.com>
2022-10-15 14:53:57 -0300

    ConvertBooleanToString

---
+++ src/main/java/org/kyu8/ConvertBooleanToString.java
@@ -0,0 +1,24 @@
+package org.kyu8;
+
+/*
+ * 8 kyu - Convert a Boolean to String
+ *
+ * https://www.codewars.com/kata/551b4501ac0447318f0009cd
+ *
+ * Implement a function which convert the given boolean value into its string representation.
+ *
+ * Note: Only valid inputs will be given.
+ *
+ */
+
+public class ConvertBooleanToString {
+
+    public static String convert(boolean b) {
+        return Boolean.toString(b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert(false));
+    }
+}
+
